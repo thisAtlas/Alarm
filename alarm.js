a596cc34c5ab95650499794881311e97abbdcccb
thisAtlas <magnus.kartvedt.p@gmail.com>
2017-04-19 08:51:56 +0200

    everything copied

--- alarm.js
+++ alarm.js
@@ -3,28 +3,16 @@ var macAddress = "00:06:66:7D:83:BF";
 function onLoad(){
 	document.addEventListener("deviceready", onDeviceReady, false);
 }
-/* Bluetooth funktionerne starter her. De er givet til os af lærer.
- */
 function onDeviceReady(){
 	bluetoothSerial.connect(macAddress, onConnect, onDisconnect);
 }
-/* I onConnect() kaldes bluetoothSerial.subscribe, der kaldes når data modtages.
- * Data skal sendes med et slut tegn. I dette eksempel er det \n, der indgår i
- * Arduino-kommandoen println().
- */
 function onConnect() {
     bluetoothSerial.subscribe("\n", onMessage, subscribeFailed);
     document.getElementByID("statusDiv").innerHTML="Connected to " + macAddress + ".";        		
 }
-/* Data modtaget fra arduinoen vises i "fraArduino".
- */
 function onMessage(data) {
-	document.getElementById("reply").innerHTML="Data: "+data;
+	document.getElementById("fraArduino").innerHTML="Data: "+data;
 }
-/* bluetoothSerial.write sender data af formen 
- * ArrayBuffer, string, array of integers, eller et Uint8Array.
- * I dette eksempel sendes en string 
- */
 function sendToArduino(data) {
         bluetoothSerial.write(data);
 }
